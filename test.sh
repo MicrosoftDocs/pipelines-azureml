5248005052f8176420b9a511112b0391f22c0cf9
Larry Franks <larryfr@microsoft.com>
2019-01-17 12:01:09 -0500

    writing

--- test.sh
+++
@@ -1,6 +0,0 @@
-echo "Adding ML extension"
-az extension add -s https://azuremlsdktestpypi.blob.core.windows.net/wheels/sdk-release/Preview/E7501C02541B433786111FE8E140CAA1/azure_cli_ml-0.1.68-py2.py3-none-any.whl --pip-extra-index-urls  https://azuremlsdktestpypi.azureedge.net/sdk-release/Preview/E7501C02541B433786111FE8E140CAA1 --yes
-
-echo "training model"
-az ml run submit -c mybai --verbose train.py > output.txt
-
